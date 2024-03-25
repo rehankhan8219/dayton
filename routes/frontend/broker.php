2d7c07db558b654dcdd20799047cbbc629b06b19
Asifali <work.asifali@gmail.com>
2024-03-25 15:43:55 +0530

    Frontend pages setup

---
+++ routes/frontend/broker.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Frontend\BrokerController;
+
+Route::group([
+    'prefix' => 'brokers',
+    'as' => 'broker.',
+], function(){
+    Route::get('/', [BrokerController::class, 'index'])->name('index');
+    Route::get('/create', [BrokerController::class, 'create'])->name('create');
+    Route::post('/store', [BrokerController::class, 'stroe'])->name('store');
+
+    Route::group([
+        'prefix' => '{broker}',
+    ], function(){
+        Route::get('/edit', [BrokerController::class, 'edit'])->name('edit');
+        Route::patch('/update', [BrokerController::class, 'update'])->name('update');
+        // Route::delete('/delete', [BrokerController::class, 'delete'])->name('delete');
+    });
+});
--- routes/frontend/member.php
+++ routes/frontend/member.php
@@ -1,14 +1,13 @@
 <?php
 
-use App\Http\Controllers\Frontend\MemberPagesController;
+use App\Http\Controllers\Frontend\MemberController;
 
 /** All route names are prefixed with 'frontend.'.
  *  These routes can only be accessed by users with type `user`
  */
 Route::group(['as' => 'member.', 'middleware' => ['auth', 'is_user']], function () {
-    Route::get('/home', [MemberPagesController::class, 'home'])->name('home');
-    Route::get('/broker-list', [MemberPagesController::class, 'brokerList'])->name('broker-list');
-    Route::get('/add-account', [MemberPagesController::class, 'addAccount'])->name('add-account');
-    Route::get('/pay-now', [MemberPagesController::class, 'payNow'])->name('pay-now');
-    Route::get('/grow-team-page', [MemberPagesController::class, 'growTeamPage'])->name('grow-team-page');
+    Route::get('/home', [MemberController::class, 'home'])->name('home');
+
+    Route::get('/pay-now', [MemberController::class, 'payNow'])->name('pay-now');
+    Route::get('/grow-team-page', [MemberController::class, 'growTeamPage'])->name('grow-team-page');
 });
